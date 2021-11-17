60a7aac64eb9e095ca56149517f437970e85c3a7
zbidi saad <zbidisaad01@gmail.com>
2021-11-17 16:07:27 +0100

    first commit

---
+++ app-swoosh/app-swoosh/View/BorderButton.swift
@@ -0,0 +1,17 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Saad Zbidi on 16/11/2021.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+}
